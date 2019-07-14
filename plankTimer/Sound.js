ea1725a8df46005ebe1e284e31e8fedec3b165eb
chavincc <cc6chavin@gmail.com>
2019-07-14 21:42:39 +0700

    sound error test-3

--- plankTimer/Sound.js
+++ plankTimer/Sound.js
@@ -2,8 +2,8 @@ class Sound {
   constructor(src) {
     this.sound = document.createElement('audio');
     this.sound.src = src;
-    this.sound.setAttribute('preload', 'auto');
-    this.sound.setAttribute('controls', 'none');
+    // this.sound.setAttribute('preload', 'auto');
+    // this.sound.setAttribute('controls', 'none');
     this.sound.style.display = 'none';
     document.body.appendChild(this.sound);
   }
